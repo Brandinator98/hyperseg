51aee39485831761c115c340c53e731d424fe734
Ann-Katrin <abrand@uni-koblenz.de>
2024-08-14 19:26:46 +0200

    make things run

--- hyperseg/models/resnet_dualencoder.py
+++ hyperseg/models/resnet_dualencoder.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import hyperseg
 
 class DualEncoderResNet(SemanticSegmentationModule):
-    def __init__(self, out_channels=25,
+    def __init__(self,
             **kwargs):
         super(DualEncoderResNet, self).__init__(**kwargs)
         #load model 
@@ -24,23 +24,22 @@ class DualEncoderResNet(SemanticSegmentationModule):
         self.encoder = torch.nn.Sequential(*list(original_model.children())[:-2])
         self.classifier = torch.nn.Sequential(original_model.classifier)
         self.aux_classifier = torch.nn.Sequential(original_model.aux_classifier)
+        self.hsi_encoder = copy.deepcopy(self.encoder)
+        self.hsi_encoder[0].conv1 = torch.nn.Conv2d(
+            in_channels=self.n_channels, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False
+        ) 
         #self.upsample = torch.nn.Upsample(size=600)
         self.upsample = torch.nn.ConvTranspose2d(in_channels=21, out_channels=21, kernel_size=16, padding=4, stride=8, output_padding=0)
-        self.conv1x1 = torch.nn.Conv2d(in_channels=21, out_channels=out_channels, kernel_size=1) #indian pines has 16 classes
-        self.hsi_initialized = False
+        self.conv1x1 = torch.nn.Conv2d(in_channels=21, out_channels=self.n_classes, kernel_size=1) #indian pines has 16 classes
         
     def forward(self, image):
         #print(f"Image shape: {image.shape}, {image.shape[2]}") # print in-between shapes
-        if (self.hsi_initialized):
-            rgb_image = image[:, [19, 8, 0], :, :]  # Extract pseudo-RGB image like Ding et al. - bands 63, 19, 1
-            y = self.hsi_encoder(image)
-            x = self.encoder(rgb_image)
-            #print(f"X shape: {np.shape(x['out'])}, Y shape: {np.shape(y['out'])}") # print in-between shapes
-            # Insert feature addition
-            z = self.add_features(x['out'], y['out'])
-        else: 
-            x = self.encoder(image)
-            z = x['out']
+        rgb_image = image[:, [19, 8, 0], :, :]  # Extract pseudo-RGB image like Ding et al. - bands 63, 19, 1
+        y = self.hsi_encoder(image)
+        x = self.encoder(rgb_image)
+        #print(f"X shape: {np.shape(x['out'])}, Y shape: {np.shape(y['out'])}") # print in-between shapes
+        # Insert feature addition
+        z = self.add_features(x['out'], y['out'])
         z = self.classifier(z)
         z = self.upsample(z)
         z = self.conv1x1(z)
@@ -49,19 +48,7 @@ class DualEncoderResNet(SemanticSegmentationModule):
     def add_features(self, x, y):
         return torch.add(x,y)
     
-    def define_hsi_encoder(self, hsi_image):
-        try:
-            self.hsi_encoder = copy.deepcopy(self.encoder)
-            self.hsi_encoder[0].conv1 = torch.nn.Conv2d(
-                in_channels=hsi_image.shape[0], out_channels=64, kernel_size=7, stride=2, padding=3, bias=False
-            ) 
-            self.hsi_initialized = True
-            self.upsample = torch.nn.Upsample(size=hsi_image.shape[2])
-            return
-        except Exception as e:
-            print(e)
-            return
-
+   
 #if __name__ == "__main__":
     # load pre-trained weights into model 
     # model = DualEncoderResNet(n_classes=25, n_channels=220, learning_rate=0.001, label_def=Path(hyperseg.__file__).parent.joinpath("datasets/labeldefs").joinpath('whuohs_labeldef.txt'), loss_name="cross_entropy", optimizer_name="AdamW", momentum=0, weight_decay=0, ignore_index=0)
