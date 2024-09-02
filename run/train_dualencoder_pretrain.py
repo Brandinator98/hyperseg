f2f7b7c780ebd2d26f46d7b73cec41e6068c3595
Ann-Katrin <abrand@uni-koblenz.de>
2024-09-02 10:46:00 +0200

    modifications

--- run/train_dualencoder_pretrain.py
+++ run/train_dualencoder_pretrain.py
@@ -104,7 +104,7 @@ def train(cfg):
         callbacks.append(
             EarlyStopping(
                 monitor="Validation/jaccard",
-                patience=15,
+                patience=30,
                 mode="max"
             )
         )
